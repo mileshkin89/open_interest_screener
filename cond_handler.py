74100f459b858ffefcc8424f9670e9a76f46b742
user <user@gmail.com>
2025-05-24 19:18:55 +0300

    feat(ListenerManager): added a listener manager. Now I can scale the number of exchanges.

--- cond_handler.py
+++ cond_handler.py
@@ -1,22 +1,22 @@
 import asyncio
-from exchange_listeners.binance_listener import BinanceListener
+from exchange_listeners.base_listener import BaseExchangeListener
 
 AVAILABLE_INTERVAL = {
-    "5m": 5,
-    "15m": 15,
-    "30m": 30,
-    "1h": 60,
-    "2h": 120,
-    "4h": 240,
-    "6h": 360,
-    "12h": 720,
-    "1d": 1440
+    "5": 5,
+    "15": 15,
+    "30": 30,
+    # "1h": 60,
+    # "2h": 120,
+    # "4h": 240,
+    # "6h": 360,
+    # "12h": 720,
+    # "1d": 1440
 }
 
-client_binance = BinanceListener()
 
 class ConditionHandler:
     def __init__(self):
+        self.client: BaseExchangeListener = None
         self.symbols = ""
         self.interval = ""
         self.limit = None
@@ -24,14 +24,20 @@ class ConditionHandler:
         self.threshold_period: int = None
 
 
+    def set_client(self, client: BaseExchangeListener):
+        self.client = client
+
+    def sort_by_timestamp(self, coin_list: list[dict]) -> list[dict]:
+        return sorted(coin_list, key=lambda x: x['timestamp'])
+
     def delta_calculate(self, data_last, data_first) -> float:
         delta = (float(data_last) - float(data_first)) / float(data_last)
         return delta
 
 
-    async def is_signal(self, symbols: list, threshold_period: int = 15, interval: str = "5m", threshold: float = 0.04):
-        self.symbols = symbols
-        self.interval = interval.lower()
+    async def is_signal(self, symbols: list, threshold_period: int = 15, interval: str = "5", threshold: float = 0.04):
+        self.symbols = symbols#[:5]
+        self.interval = interval
         self.threshold_period = threshold_period
         self.limit = int(self.threshold_period / AVAILABLE_INTERVAL[self.interval])
         self.threshold = threshold
@@ -40,40 +46,47 @@ class ConditionHandler:
         print("threshold_period = ",  self.threshold_period)
         print(f"threshold = {self.threshold*100}%")
 
-        tasks = [client_binance.fetch_oi(symbol.upper(), self.interval, self.limit) for symbol in self.symbols]
+        tasks = [self.client.fetch_oi(symbol.upper(), self.interval, self.limit) for symbol in self.symbols]
         coins = await asyncio.gather(*tasks)
 
         for coin in coins:
+            coin = self.sort_by_timestamp(coin)
+            #print("coin = ", coin)
+
             for i in range(1,len(coin)):
-                delta_oi = self.delta_calculate(coin[i]['OpenInterest'], coin[0]['OpenInterest']) #(float(coin[i]['OpenInterest']) - float(coin[0]['OpenInterest'])) / float(coin[i]['OpenInterest'])
+                delta_oi = self.delta_calculate(coin[i]['OpenInterest'], coin[0]['OpenInterest'])
                 formatted_delta_oi = f"{delta_oi*100:.2f}%"
 
                 if delta_oi > self.threshold:
 
                     start_date = coin[0]['timestamp']
                     end_date = coin[i]['timestamp']
+                    exchange_name = coin[i]['exchange']
 
-                    signal_coin_price_volume = await client_binance.fetch_ohlcv(coin[0]['symbol'], self.interval, start_date, end_date)
+                    signal_coin_price_volume = await self.client.fetch_ohlcv(coin[0]['symbol'], start_date, end_date, str(self.interval))
 
-                    delta_price = self.delta_calculate(signal_coin_price_volume[-1]['close'], signal_coin_price_volume[0]['close'])
-                    delta_volume = self.delta_calculate(signal_coin_price_volume[-1]['volume'], signal_coin_price_volume[0]['volume'])
-                    formatted_delta_price = f"{delta_price * 100:.2f}%"
-                    formatted_delta_volume = f"{delta_volume * 100:.2f}%"
+                    print("signal_coin_price_volume = ",signal_coin_price_volume)
 
-                    delta_time = coin[i]['datetime'] - coin[0]['datetime']
-                    delta_minutes = delta_time.total_seconds() / 60
+                    if signal_coin_price_volume and len(signal_coin_price_volume) >= 2:
+                        delta_price = self.delta_calculate(signal_coin_price_volume[-1]['close'], signal_coin_price_volume[0]['close'])
+                        delta_volume = self.delta_calculate(signal_coin_price_volume[-1]['volume'], signal_coin_price_volume[0]['volume'])
+                        formatted_delta_price = f"{delta_price * 100:.2f}%"
+                        formatted_delta_volume = f"{delta_volume * 100:.2f}%"
 
-                    is_signal_coin = {
-                        'exchange': 'Binance',
-                        'symbol': coin[i]['symbol'],
-                        'timestamp': coin[i]['timestamp'],
-                        'datetime': coin[i]['datetime'].strftime("%Y-%m-%d %H:%M:%S"),
-                        'delta_oi_%': formatted_delta_oi,
-                        'delta_price_%': formatted_delta_price,
-                        'delta_volume_%': formatted_delta_volume,
-                        'delta_time_minutes': delta_minutes,
-                    }
-                    signal_coins.append(is_signal_coin)
+                        delta_time = coin[i]['datetime'] - coin[0]['datetime']
+                        delta_minutes = delta_time.total_seconds() / 60
+
+                        is_signal_coin = {
+                            'exchange': exchange_name,
+                            'symbol': coin[i]['symbol'],
+                            'timestamp': coin[i]['timestamp'],
+                            'datetime': coin[i]['datetime'],
+                            'delta_oi_%': formatted_delta_oi,
+                            'delta_price_%': formatted_delta_price,
+                            'delta_volume_%': formatted_delta_volume,
+                            'delta_time_minutes': delta_minutes,
+                        }
+                        signal_coins.append(is_signal_coin)
                     break
 
         return signal_coins
---
+++ exchange_listeners/listener_manager.py
@@ -0,0 +1,30 @@
+from exchange_listeners.binance_listener import BinanceListener
+from exchange_listeners.bybit_listener import BybitListener
+from typing import Any
+
+class ListenerManager:
+    def __init__(self, enabled_exchanges: list[str]):
+        self.enabled_exchanges = [e.lower() for e in enabled_exchanges]
+        self.exchange_map = {
+            "binance": BinanceListener(),
+            "bybit": BybitListener(),
+        }
+
+    def get_listener(self, exchange_name: str) -> Any | None:
+        exchange_name = exchange_name.lower()
+        try:
+            if exchange_name in self.enabled_exchanges:
+                return self.exchange_map[exchange_name]
+            else:
+                raise ValueError(f"Exchange '{exchange_name}' is not activated in ListenerManager.")
+        except ValueError as e:
+            print(f"Exchange not available. {e}")
+
+    def get_all_active_listeners(self) -> list[dict]:
+        return [{name: self.exchange_map[name]} for name in self.enabled_exchanges]
+
+    def get_all_active_names(self) -> list[str]:
+        return self.enabled_exchanges
+
+    def get_all_listeners(self) -> dict:
+        return self.exchange_map
