296e868206b8a0ee0b5e85dc7608f67cbce678a0
user <user@gmail.com>
2025-05-26 15:34:06 +0300

    feat(db): connect db - aiosqlite

--- config.py
+++ config.py
@@ -1,8 +1,26 @@
 # config
 
+from pathlib import Path
+from pydantic_settings import BaseSettings, SettingsConfigDict
 from dotenv import load_dotenv
 import os
 
+
+BASE_DIR = Path(__file__).parent
+
 load_dotenv()
 
-TG_BOT_API_KEY=os.getenv("TG_BOT_API_KEY")
+
+class AppConfig(BaseSettings):
+    DB_PATH: Path = BASE_DIR / "storage" / "signals.db"
+    DB_PATH.parent.mkdir(parents=True, exist_ok=True)
+
+    TG_BOT_API_KEY: str = os.getenv("TG_BOT_API_KEY")
+
+    model_config = SettingsConfigDict(
+        env_file=str(BASE_DIR / ".env"),
+        env_file_encoding="utf-8"
+    )
+
+
+config = AppConfig()
